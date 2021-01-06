f24b17c4c26d46e95c40f396ed47e5e917a4c005
Emerson <emerson@cloudfour.com>
2021-01-06 14:50:56 -0800

    ByTitle tests

---
+++ tests/testing-library-queries/ByTitle.test.ts
@@ -0,0 +1,21 @@
+import { createTab } from 'test-mule';
+
+test('ByTitle', async () => {
+  const { screen, utils } = await createTab();
+  await utils.injectHTML(`
+    <img title="Foo" />
+    <img title="Foobar" />
+    <img />
+  `);
+  // finds one
+  await screen.getByTitle('Foobar');
+  // too many
+  await expect(screen.getByTitle(/Foo/)).rejects.toThrow(
+    'Found multiple elements',
+  );
+  expect(await screen.getAllByTitle(/Foo/)).toHaveLength(2);
+  // doesn't find any
+  await expect(screen.getByTitle('Baz')).rejects.toThrow(
+    'Unable to find',
+  );
+});
