6b53bdb071c6b5b274bf05b46d932e20af21b84c
Caleb Eby <caleb.eby01@gmail.com>
2021-01-06 17:03:36 -0800

    Add lint script (prettier) to package.json

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    '@babel/preset-typescript'
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
   ],
-  plugins: ["@babel/plugin-transform-modules-commonjs"],
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
 };
