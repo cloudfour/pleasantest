7cfcc99bc1b71e4bc069d90a279b6ac831ab3b79
Emerson <emerson@cloudfour.com>
2021-01-06 16:23:51 -0800

    ByDisplayValue tests

---
+++ tests/testing-library-queries/ByDisplayValue.test.ts
@@ -0,0 +1,26 @@
+import { createTab } from 'test-mule';
+
+test('ByDisplayValue', async () => {
+  const { screen, utils } = await createTab();
+  await utils.injectHTML(`
+    <input value="Foo" />
+    <input value="Fo" />
+    <textarea>Bar</textarea>
+    <select>
+      <option value="1" selected>Apple</option>
+      <option value="2">Banana</option>
+    </select>
+  `);
+  // finds one
+  await screen.getByDisplayValue('Foo');
+  await screen.getByDisplayValue('Bar');
+  await screen.getByDisplayValue('Apple');
+  // too many
+  await expect(screen.getByDisplayValue(/Fo/)).rejects.toThrow(
+    'Found multiple elements',
+  );
+  // doesn't find any
+  await expect(screen.getByDisplayValue('Cheeseburger')).rejects.toThrow(
+    'Unable to find',
+  );
+});
