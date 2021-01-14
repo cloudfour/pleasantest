d7e552a15269dc490d26b2cc492e67bc57a96341
Caleb Eby <caleb.eby01@gmail.com>
2021-01-14 14:50:54 -0800

    Fix toBeInTheDocument

--- tests/jest-dom-matchers/toBeInTheDocument.test.ts
+++ tests/jest-dom-matchers/toBeInTheDocument.test.ts
@@ -1,10 +1,12 @@
 import { createTab } from 'test-mule';
 
-test.skip('toBeInTheDocument', async () => {
+test('toBeInTheDocument', async () => {
   const { screen, utils } = await createTab();
   await utils.injectHTML(`<button>Hi</button>`);
   const inDoc = await screen.getByText('Hi');
   await expect(inDoc).toBeInTheDocument();
-  await inDoc.parentNode.removeChild(inDoc);
-  await expect(expect(inDoc).toBeInTheDocument()).rejects.toThrow('foo');
+  await inDoc.evaluate((el) => el.remove());
+  await expect(expect(inDoc).toBeInTheDocument()).rejects.toThrow(
+    'element could not be found in the document',
+  );
 });
