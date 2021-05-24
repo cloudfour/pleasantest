1266b5a683360efadebe1054bfdb714f745bd07c
Caleb Eby <caleb.eby01@gmail.com>
2021-05-24 15:20:17 -0700

    Typo

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   },
   // Don't transform node_modules, _except_ ansi-regex
   // ansi-regex is ESM and since we are jusing Jest in CJS mode,
-  // it must be transpiled to ESM
+  // it must be transpiled to CJS
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!ansi-regex)'],
 };
