8ba3a7c68cd982a04e97e3320859ba61fb3fa66c
Caleb Eby <caleb.eby01@gmail.com>
2021-05-24 15:10:43 -0700

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
   testRunner: 'jest-circus/runner',
   watchPathIgnorePatterns: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.tsx?$': ['esbuild-jest', { sourcemap: true }],
+    '^.+\\.[jt]sx?$': ['esbuild-jest', { sourcemap: true }],
   },
+  // Don't transform node_modules, _except_ ansi-regex
+  // ansi-regex is ESM and since we are jusing Jest in CJS mode,
+  // it must be transpiled to ESM
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!ansi-regex)'],
 };
