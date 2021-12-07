c1578a243f359b53682f3cdbc8af870171f96e64
Caleb Eby <caleb.eby01@gmail.com>
2021-12-07 15:41:01 -0800

    Fix indentation in svelte file

--- tests/utils/svelte-component.svelte
+++ tests/utils/svelte-component.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   // Check for TS support
-	let count: number = 0;
+  let count: number = 0;
 
-	function handleClick() {
-		count += 1;
-	}
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <button on:click={handleClick}>
-	Clicked {count} {count === 1 ? 'time' : 'times'}
+  Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
 
 <style lang="scss">
